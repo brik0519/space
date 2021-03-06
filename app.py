8249a10c94e53d3dfa86edcc7127b75b09f3e870
Brian Kim <brik0519@gmail.com>
2021-03-06 10:47:12 +0000

    remove log function

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template
 import sys
 
-from logging.handlers import RotatingFileHandler
-import logging
-
-logging.basicConfig(filename = "server.log", level = logging.INFO)
-application=Flask(__name__)
-
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
