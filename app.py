8ccb6aef2eacfc0d860efbfdd37a4e79f9582f8a
Brian Kim <brik0519@gmail.com>
2021-03-09 16:56:07 +0000

    add atm subpage

--- app.py
+++ app.py
@@ -10,11 +10,22 @@ def index():
     return render_template("/index.html")
 
 
-# Notion Widgets
+# mdforest
+## pointer
 @app.route('/pointer/')
 def pointer():
     return render_template("/widget/pointer.html")
 
+## postbox
 @app.route('/postbox/')
 def postbox():
     return render_template("/postbox.html")
+
+## atm
+@app.route('/atm/')
+def atm():
+    return render_template("/atm/atm.html")
+
+@app.route('/pay/')
+def pay():
+    return render_template("/atm/pay.html")
