9803954c74f10a398b3bf4ac525ffd8ce75dd2ee
Brian Kim <brik0519@gmail.com>
2021-03-11 18:24:01 +0000

    fin

--- app.py
+++ app.py
@@ -16,10 +16,16 @@ def index():
 def pointer():
     return render_template("/widget/pointer.html")
 
+
 ## postbox
 @app.route('/postbox/')
 def postbox():
-    return render_template("/postbox.html")
+    return render_template("/postbox/postbox.html")
+
+@app.route('/received/')
+def received():
+    return render_template("/postbox/received.html")
+
 
 ## atm
 @app.route('/atm/')
@@ -29,3 +35,13 @@ def atm():
 @app.route('/pay/')
 def pay():
     return render_template("/atm/pay.html")
+
+
+## message
+@app.route('/message/')
+def message():
+    return render_template("/message.html")
+
+@app.route('/time/')
+def time():
+    return render_template("/time.html")
